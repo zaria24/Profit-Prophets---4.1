aa762923da501200519107b28605388fbe38bd98
harlemmorton <92065951+harlemmorton@users.noreply.github.com>
2024-11-12 20:56:03 -0500

    Add files via upload
    This all of the loose files aka the ones outside of a folder on the flash drive (11/12) after we tested the .exe file and it runs on Dr. Williams and Jayden's computer.

---
+++ config.py
@@ -0,0 +1,14 @@
+# To get started with Profit Prophet's Auto Trading Stock Bot, CREATE AN ALPACA TRADING ACCOUNT AND REQUEST A NEW API KEY AND NEW SECRET KEY
+
+
+API_KEY = 'PK5G7DZ66JE1ZW1FTFN6'
+SECRET_KEY = 'kKsoaBRjdMbS4TS7ARbEy4gTjCs3c7oxDiNnWEXf'
+
+'''
+ALPACA_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS - {
+     'PKLGC84PSXPHM79WWULX': API_KEY,
+     'c15VhM0cSZ1bIz7BgfAngLdlpjJ3khFYDoGobyIf': SECRET_KEY
+}
+'''
